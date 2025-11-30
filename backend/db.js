1e6094acae06da10d7224d44112a0e533b88e85e
synditrix <synditrix@gmail.com>
2025-11-30 16:29:59 -0500

    added db.js file

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+// db.js
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
+
+// Optional: basic startup test + nice error logging
+pool.on("error", (err) => {
+    console.error("Unexpected PG client error", err);
+});
+
+export default pool;
