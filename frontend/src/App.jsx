332b9c85068c7abd0a273bfc5db0fda00c3c6e84
synditrix <synditrix@gmail.com>
2025-12-01 10:53:36 -0500

    stylized the stats cards a bit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { StatsOverview } from './components/StatsOverview';
 function App() {
     return (
         <div style={{ padding: '1.5rem' }}>
+            <header> Overview </header>
             <StatsOverview />
             {/* later: Recharts components  */}
         </div>
